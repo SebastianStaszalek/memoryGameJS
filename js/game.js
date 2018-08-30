808130907b35d77e6e02883e043c45603c7c74e5
SebastianStaszalek <sebastian.staszalek@gmail.com>
2018-08-30 20:23:51 +0200

    starts game with good num of pieces,

--- js/game.js
+++ js/game.js
@@ -1,28 +1,63 @@
+'use strict';
+
+// var Piece = function (id) {
+//     this.toGuess = false;
+//     this.id = id;
+// };
+
 var game = (function () {
 
-    var initialNumberOfPieces = 5,
+
+    var initialNumberOfPieces = 4,
         currentNumberOfPieces,
+        pieces = [],
+
         startGame = function (config) {
             if (config && config.numberOfPieces) {
                 currentNumberOfPieces = config.numberOfPieces;
             } else {
                 currentNumberOfPieces = initialNumberOfPieces;
             }
+            placePieces();
+        },
+
+        getNumberOfPieces = function () {
+            return currentNumberOfPieces;
+        },
+
+        placePieces = function () {
+            var i,
+                randomPosition;
+                pieces = [];
+
+            for (i = 0; i < currentNumberOfPieces; i++) {
+                pieces.push({});
+                pieces[i].toGuess = false;
+            }
+
+            randomPosition = getRandomPosition(currentNumberOfPieces);
+            if(pieces[randomPosition].toGuess === false) {
+                pieces[randomPosition].toGuess = true;
+            }
+
+            return pieces;
+
+        },
+
+        getRandomPosition = function(number) {
+            return Math.floor(Math.random() * number);
         },
 
         getPieces = function () {
-            var i,
-                pieces = [];
-
-            for(i=0; i < currentNumberOfPieces; i++) {
-                pieces.push({});
-            }
-            pieces[0].toGuess = true;
             return pieces;
         };
 
+
+
+
     return {
         'startGame': startGame,
+        'getNumberOfPieces': getNumberOfPieces,
         'getPieces': getPieces
     }
 })();
--- test/game.test.js
+++ test/game.test.js
@@ -1,12 +1,12 @@
 describe('Game', function () {
-    it('should have 5 pieces after game start', function () {
+    it('should have 4 pieces after game start', function () {
         var pieces;
 
         game.startGame();
 
         pieces = game.getPieces();
 
-        expect(pieces.length).toBe(5);
+        expect(pieces.length).toBe(4);
     });
 
     it('one pieces should be to guess after game start', function () {
@@ -16,6 +16,30 @@ describe('Game', function () {
         piecesToGuess = findPiecesToGuess(game.getPieces());
 
         expect(piecesToGuess.length).toBe(1);
+    });
+
+    it('two pieces should be to guess when number of pieces is 6', function () {
+        var piecesToGuess,
+            config = {
+                numberOfPieces: 6
+            };
+        game.startGame(config);
+
+        piecesToGuess = findPiecesToGuess(game.getPieces());
+
+        expect(piecesToGuess.length).toBe(2);
+    });
+
+    it('three pieces should be to guess when number of pieces is 8', function () {
+        var piecesToGuess,
+            config = {
+                numberOfPieces: 8
+            };
+        game.startGame(config);
+
+        piecesToGuess = findPiecesToGuess(game.initializePieces());
+
+        expect(piecesToGuess.length).toBe(3);
     });
 
     it('should start game with configured number of pieces', function () {

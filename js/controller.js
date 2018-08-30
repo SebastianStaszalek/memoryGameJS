d41b481e4043cb2094fe06cd6c3698acd3fab353
SebastianStaszalek <sebastian.staszalek@gmail.com>
2018-08-30 23:17:13 +0200

    restart level and get next level

--- js/controller.js
+++ js/controller.js
@@ -1,16 +1,40 @@
+'use strict';
 var controller = function () {
     var startGame = function () {
-        var initialNumberOfPieces = view.getInitialNumberOfPieces();
+            var initialNumberOfPieces = view.getInitialNumberOfPieces();
 
-        game.startGame({
-            numberOfPieces: initialNumberOfPieces
-        });
+            game.startGame({
+                numberOfPieces: initialNumberOfPieces
+            });
+            view.renderPieces(initialNumberOfPieces);
+        },
 
-        view.renderPieces(game.getPieces());
 
-    };
+
+        getNumberOfPieces = function() {
+            return game.getNumberOfPieces();
+        },
+
+        getPieces = function() {
+            return game.getPieces();
+        },
+
+        nextLevel = function () {
+            game.nextLevel();
+            view.renderPieces(game.getPieces());
+        },
+
+        restartLevel = function () {
+            game.restartLevel();
+            view.renderPieces(game.getPieces());
+        };
 
     return {
-        'startGame': startGame
+        'startGame': startGame,
+        'getNumberOfPieces': getNumberOfPieces,
+        'getPieces': getPieces,
+        'nextLevel': nextLevel,
+        'restartLevel': restartLevel
+
     }
 }();
--- js/view.js
+++ js/view.js
@@ -1,9 +1,57 @@
+'use strict';
+
 var view = (function () {
+
     var getInitialNumberOfPieces = function () {
-        //donm
-    };
+            return document.getElementById("initialNumberOfPieces").value;
+        },
+
+        renderPieces = function () {
+            var memoryGame = document.getElementById("memorygame"),
+                i,
+                id,
+                piece,
+                pieces;
+
+            clearGame();
+
+            pieces = controller.getPieces();
+
+            for (i = 0; i < pieces.length; i++) {
+                id = "piece" + i;
+                piece = document.createElement("div");
+                if(pieces[i].toGuess === true) {
+                    piece.setAttribute("class", "highlight");
+                }
+
+                piece.setAttribute("id", id);
+                memoryGame.appendChild(piece);
+            }
+        },
+
+        clearGame = function () {
+            var piece;
+            while (document.getElementById("memorygame").hasChildNodes()) {
+                piece = document.getElementById("memorygame").firstChild;
+                document.getElementById("memorygame").removeChild(piece);
+            }
+        },
+
+        highlightPieces = function (pieces) {
+            var i,
+            piece;
+            for (i = 0; i < pieces.length; i++) {
+                if (pieces[i].toGuess === true) {
+                    piece = document.getElementById("piece" + i);
+                    piece.setAttribute("class", "highlight");
+                }
+            }
+        };
+
 
     return {
-        'getInitialNumberOfPieces': getInitialNumberOfPieces
+        'renderPieces': renderPieces,
+        'getInitialNumberOfPieces': getInitialNumberOfPieces,
+        'highlightPieces': highlightPieces
     }
 })();
